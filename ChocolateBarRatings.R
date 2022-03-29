599d53f25a0555a780db8ed3a59ac82ff64d460b
Toni Rabasco <tonitonitone@Tonis-Air.home>
2022-03-28 21:14:45 -0400

    data graphics

--- ChocolateBarRatings.R
+++ ChocolateBarRatings.R
@@ -27,7 +27,7 @@
 # # Percent` <- sapply(chocolateData$Cocoa_Percent, function(x) gsub("%","",x)) 
 # 
 # chocolateData <- type.convert(chocolateData)
-
+?group_by
 library(tidyverse)
 library(dplyr)
 library(stringr)
@@ -40,8 +40,37 @@ choc
 names(choc) <- gsub("[[:space:]+]", "_", names(choc))
 str(choc) #check the data type of the columns in the chococlateData
 ### return a data frame with the mean and sd of the rating column from chocolate dataset
-choc %>% 
+library(dplyr)
+choc %>%
   summarise(averageRating = mean(Rating),
-            sdRating = sd(Rating),)
-   
-  
+            sdRating = sd(Rating)) 
+### return the average and sd ratings by the year a rating was given
+choc %>% 
+  group_by(ReviewDate) %>% 
+    summarise(averageRating = mean(Rating),
+                  sdRating = sd(Rating))
+###return dataframe with average and sd CocoaPercent1 by the year review 
+choc %>% 
+  group_by(ReviewDate)
+    summarise(averageCocoaPercent = mean(CocoaPercent),
+              sdCocoaPercent = sd(CocoaPercent))
+#Graphing the data
+library(ggplot2)
+ggplot(choc, aes(x= ReviewDate, y= Rating))
+###plot review date on x axis and rating on y axis and add point for each data point 
+ggplot(choc, aes(x=ReviewDate, y=Rating)) + geom_point() + geom_jitter() # the jitter makes it so each point is not on top of each other
+###adding a smooth line to test for linearity 
+ggplot(choc, aes(x=ReviewDate, y=Rating)) + 
+  geom_point() + 
+  geom_jitter() + 
+  geom_smooth(method = 'lm')
+#encoding cocoapercent as a color   
+ggplot(choc, aes(x=ReviewDate, y=Rating, color= CocoaPercent)) + 
+  geom_point() + 
+  geom_jitter() + 
+  geom_smooth(method = 'lm')
+#relationship between cocoa percentage and ratings 
+ggplot(choc, aes(x=CocoaPercent, y=Rating, color= CocoaPercent)) + 
+  geom_point() + 
+  geom_jitter() + 
+  geom_smooth(method = 'lm')
